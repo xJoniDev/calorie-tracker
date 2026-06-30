a18022f6d1885d88ee3f600aea3e16784ec641f7
JoniQP <jonathansigwart55@gmail.com>
2026-06-30 16:19:43 +0200

    Added more features

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Calorie Tracker script loaded.");
+
+function login () {
+    const username = document.getElementById("email-login").value;
+    const password = document.getElementById("password-login").value;
+    console.log("Loggin in...");
+    window.location.href = "calorie-tracker.html";
+}
